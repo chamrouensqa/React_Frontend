8eea4b9a6a2a8236ae7ae0623270158b30578896
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-06-09 15:19:48 +0700

    Add redirect link when user click the addition url

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Users from './users/pages/Users';
 
 const App = () => {
   return (
     <Router>
-      <Route path='/'>
+      <Route path='/'exact>
         <Users/>
       </Route>
+      <Redirect to="/"/>
   </Router>
   );
 };
