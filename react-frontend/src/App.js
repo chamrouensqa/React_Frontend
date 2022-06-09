7b2fba8febc50ba5bd87e282c5abe1c38f76a693
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-06-09 15:42:57 +0700

    Adding switch routes

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import { Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
-      <Route path='/'exact>
-        <Users/>
-      </Route>
-      <Redirect to="/"/>
+      <Switch>
+        <Route path='/'exact>
+          <Users/>
+        </Route>
+        <Route path="/places/new1" exact>
+          <NewPlace/>
+        </Route>
+        <Redirect to="/"/>
+      </Switch>
   </Router>
   );
 };
---
+++ react-frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewPlace = () =>{
+    return <h1>New Place in Cambodia</h1>
+};
+
+export default NewPlace;
